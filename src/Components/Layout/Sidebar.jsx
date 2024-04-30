1c69b7d5ee817df750c7ff3476f711c9e826c222
Hero Coders <153440280+tamim-devs@users.noreply.github.com>
2024-04-30 08:50:10 +0600

    barta

--- src/Components/Layout/Sidebar.jsx
+++ src/Components/Layout/Sidebar.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
+import '../Layout/Layout.css'
+import { Avatar } from '@mui/material'
+import { Link, NavLink } from 'react-router-dom'
+import { IoHomeOutline } from "react-icons/io5";
+import { AiOutlineMessage } from "react-icons/ai";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { MdOutlineSettings } from "react-icons/md";
+import { FaDoorOpen } from "react-icons/fa";
+
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className='sidebarmain'>
+          <div className='sidebar_inner'>
+            <div className='profileImg'>
+            <Avatar  alt="Remy Sharp" src="/static/images/avatar/1.jpg" sx={{ width: 100, height: 100 }} />
+            </div>
+
+                <div style={{}}>
+                    <ul className='sidebarUl'>
+
+                      <li>
+                        <NavLink>
+                        <IoHomeOutline  className=''/>
+                        </NavLink>
+                        </li>
+
+                        <li>
+                        <NavLink>
+                        <AiOutlineMessage  className=''/>
+                        </NavLink>
+                        </li>
+
+                        <li>
+                        <NavLink>
+                        <IoIosNotificationsOutline  className=''/>
+                        </NavLink>
+                        </li>
+
+                        <li>
+                        <NavLink>
+                        <MdOutlineSettings  className=''/>
+                        </NavLink>
+                        </li>
+                                      
+                    </ul>
+                </div>
+                <div style={{display: "flex", justifyContent: "center"}}>
+                      <Link className='logoutBtn'><FaDoorOpen /></Link>
+                </div>
+          
+          </div>
+    </div>
   )
 }
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+          
+         <h1>home</h1>   
+
+    </div>
   )
 }
 
